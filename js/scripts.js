8416d0434f8d0cf753b18b87a1a54664b98ed801
djimenezchocron <djimenezweb@gmail.com>
2023-01-17 22:20:38 +0100

    Calendario

--- js/scripts.js
+++ js/scripts.js
@@ -19,19 +19,34 @@ const leapYear = year => {
   }
 };
 
+// const daysOfMonth = {
+//   January: 31,
+//   February: leapYear(currentYear) ? 29 : 28,
+//   March: 31,
+//   April: 30,
+//   May: 31,
+//   June: 30,
+//   July: 31,
+//   August: 31,
+//   September: 30,
+//   October: 31,
+//   November: 30,
+//   December: 31
+// };
+
 const daysOfMonth = {
-  January: 31,
-  February: leapYear(currentYear) ? 29 : 28,
-  March: 31,
-  April: 30,
-  May: 31,
-  June: 30,
-  July: 31,
-  August: 31,
-  September: 30,
-  October: 31,
-  November: 30,
-  December: 31
+  0: 31,
+  1: leapYear(currentYear) ? 29 : 28,
+  2: 31,
+  3: 30,
+  4: 31,
+  5: 30,
+  6: 31,
+  7: 31,
+  8: 30,
+  9: 31,
+  10: 30,
+  11: 31
 };
 
 const daysOfWeek = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
@@ -47,11 +62,31 @@ const createCalendarHead = () => {
 };
 
 const createCalendar = () => {
-  const newDiv = document.createElement('div');
-  newDiv.textContent = '1';
-  newDiv.classList.add('first-day');
-  rootStyles.setProperty('first-day', '0');
-  calendarElement.prepend(newDiv);
+  const fragment = document.createDocumentFragment();
+  for (let day = 1; day <= daysOfMonth[currentMonth]; day++) {
+    const newDiv = document.createElement('div');
+    newDiv.textContent = day;
+    newDiv.classList.add('day');
+    if (day === 1) {
+      newDiv.classList.add('first-day');
+    }
+    if (day < currentDay) {
+      newDiv.classList.add('disabled');
+    } else if (day === currentDay) {
+      newDiv.classList.add('today');
+    }
+    fragment.append(newDiv);
+  }
+  calendarElement.append(fragment);
+};
+
+const setFirstDay = () => {
+  const date2 = new Date(currentYear, currentMonth, 1);
+  if (date2.getDay() === 0) {
+    return 7;
+  } else return date2.getDay();
 };
 
 createCalendarHead();
+rootStyles.setProperty('--first-day-column', setFirstDay());
+createCalendar();
